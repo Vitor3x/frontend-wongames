2fe102e8022f7f2aff0a3841609d315d6810d34a
Joao Vitor <viictorsouza97@gmail.com>
2022-09-21 22:52:42 -0300

    test: render a black logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -11,4 +11,12 @@ describe('<Logo />', () => {
       color: '#FAFAFA'
     })
   })
+
+  it('should render a black logo when color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
 })
