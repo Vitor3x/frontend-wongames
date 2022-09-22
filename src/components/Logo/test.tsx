206c3610c5434b8954324bca83a74c28bf7a2aa5
Joao Vitor <viictorsouza97@gmail.com>
2022-09-21 23:04:43 -0300

    test: add bigger logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -27,4 +27,12 @@ describe('<Logo />', () => {
       width: '11rem'
     })
   })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
 })
