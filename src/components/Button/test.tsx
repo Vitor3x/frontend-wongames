ca3c711dbe029666f355faaf3722fdfce207007b
Joao Vitor <viictorsouza97@gmail.com>
2022-09-23 21:41:38 -0300

    test: render medium button by default

---
+++ src/components/Button/test.tsx
@@ -0,0 +1,16 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+
+import Button from '.'
+
+describe('<Button />', () => {
+  it('should render the medium size by default', () => {
+    renderWithTheme(<Button>Buy Now</Button>)
+
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
+  })
+})
