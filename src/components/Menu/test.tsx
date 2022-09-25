c4f038fc92f3ad57fa07bb5aadddd973d2f69c45
Joao Vitor <viictorsouza97@gmail.com>
2022-09-25 16:48:11 -0300

    test: add show register box when logged out

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -29,4 +29,14 @@ describe('<Menu />', () => {
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.queryByText(/Log in now/i)).toBeInTheDocument()
+    expect(screen.queryByText(/Sign Up/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/My Account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Wishlist/i)).not.toBeInTheDocument()
+  })
 })
