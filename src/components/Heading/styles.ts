2815dd4c29d1b5692dcc9bf1465f939cc7746b4b
Joao Vitor <viictorsouza97@gmail.com>
2022-09-26 22:15:13 -0300

    feat: add heading sizes small and medium

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -3,6 +3,22 @@ import media from 'styled-media-query'
 import { HeadingProps } from '.'
 
 const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.medium};
+
+    &::after {
+      width: 3rem;
+    }
+  `,
+
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge}
+    `}
+  `,
+
   lineLeft: (theme: DefaultTheme) => css`
     padding-left: ${theme.spacings.xxsmall};
     border-left: 0.7rem solid ${theme.colors.secondary};
@@ -24,7 +40,7 @@ const wrapperModifiers = {
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom }) => css`
+  ${({ theme, color, lineLeft, lineBottom, size }) => css`
     color: ${theme.colors[color!]};
     font-size: ${theme.font.sizes.xlarge};
 
@@ -34,5 +50,6 @@ export const Wrapper = styled.h2<HeadingProps>`
 
     ${lineLeft && wrapperModifiers.lineLeft(theme)};
     ${lineBottom && wrapperModifiers.lineBottom(theme)};
+    ${!!size && wrapperModifiers[size](theme)}
   `}
 `
